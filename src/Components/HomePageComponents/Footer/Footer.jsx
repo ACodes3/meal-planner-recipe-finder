9b106eb6491e058f826e069dddbe3b377ee9f9de
Aspasija <aspasija.cvetkoska@result.si>
2024-07-07 23:08:49 +0200

    changed font and footer

--- src/Components/HomePageComponents/Footer/Footer.jsx
+++ src/Components/HomePageComponents/Footer/Footer.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="text-center text-lg-start bg-body-tertiary text-muted">
+    <footer className="text-center text-lg-start bg-body-tertiary text-muted"  style={{ fontFamily: "'Oswald', sans-serif" }}>
       {/* Section: Social media */}
       <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         {/* Left */}
@@ -45,11 +45,13 @@ const Footer = () => {
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               {/* Content */}
               <h6 className="text-uppercase fw-bold mb-4">
-                <i className="fas fa-gem me-3"></i>Company name
+                React Recipe finder and Meal
+                Planner
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit.
+                Discover a variety of recipes to suit every taste and
+                preference. Customize your meal plan and enjoy delicious,
+                healthy meals every day.
               </p>
             </div>
             {/* Grid column */}
@@ -57,20 +59,26 @@ const Footer = () => {
             {/* Grid column */}
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               {/* Links */}
-              <h6 className="text-uppercase fw-bold mb-4">
-                Products
-              </h6>
+              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Angular</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  React
+                </a>
               </p>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">React</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  Bootstrap
+                </a>
               </p>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Vue</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  Spoonacular API
+                </a>
               </p>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Laravel</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  FE DEV Project
+                </a>
               </p>
             </div>
             {/* Grid column */}
@@ -78,20 +86,26 @@ const Footer = () => {
             {/* Grid column */}
             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
               {/* Links */}
-              <h6 className="text-uppercase fw-bold mb-4">
-                Useful links
-              </h6>
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Pricing</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  Pricing
+                </a>
               </p>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Settings</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  Settings
+                </a>
               </p>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Orders</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  Orders
+                </a>
               </p>
               <p>
-                <a href="#!" className="text-reset text-decoration-none">Help</a>
+                <a href="#!" className="text-reset text-decoration-none">
+                  Help
+                </a>
               </p>
             </div>
             {/* Grid column */}
@@ -100,13 +114,18 @@ const Footer = () => {
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               {/* Links */}
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-              <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
               <p>
-                <i className="fas fa-envelope me-3"></i>{" "}
-                info@example.com
+                <i className="fas fa-home me-3"></i> New York, NY 10012, US
               </p>
-              <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-              <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+              <p>
+                <i className="fas fa-envelope me-3"></i> info@example.com
+              </p>
+              <p>
+                <i className="fas fa-phone me-3"></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className="fas fa-print me-3"></i> + 01 234 567 89
+              </p>
             </div>
             {/* Grid column */}
           </div>
@@ -116,14 +135,22 @@ const Footer = () => {
       {/* Section: Links  */}
 
       {/* Copyright */}
-      <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
         © 2021 Copyright:{" "}
-        <a className="text-reset fw-bold text-decoration-none" href="https://aspasija.com/">Aspasija Cvetkoska</a>
+        <a
+          className="text-reset fw-bold text-decoration-none"
+          href="https://aspasija.com/"
+        >
+          Aspasija Cvetkoska
+        </a>
       </div>
       {/* Copyright */}
     </footer>
     /* Footer */
   );
-}
+};
 
 export default Footer;
