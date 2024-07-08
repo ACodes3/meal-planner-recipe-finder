18545fa0fa71cbfc25936fc52c529f404ade9790
Aspasija <aspasija.cvetkoska@result.si>
2024-07-08 14:08:33 +0200

    added recipe page header

--- src/Components/RecipePageComponents/RecipePageHeader/RecipePageHeader.jsx
+++ src/Components/RecipePageComponents/RecipePageHeader/RecipePageHeader.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Image from "react-bootstrap/Image";
+import HeroImg from "../../../Images/fresh-vegetable-salad-with-lambs-lettuce-baked-butternut-squash-pumpkin-avocado.jpg";
 
 const RecipePageHeader = () => {
   return (
-    <div>RecipePageHeader</div>
+    <div
+      className="position-relative"
+      style={{ height: "300px", overflow: "hidden" }}
+    >
+      <Image
+        src={HeroImg}
+        className="w-100 h-100 position-absolute top-0 start-0"
+        style={{ objectFit: "cover" }}
+        rounded
+      />
+
+      <div className="position-absolute top-50 start-0 end-50 translate-middle-y p-4 text-black" style={{ fontFamily: "'Oswald', sans-serif" }}>
+        <div className="text-left">
+          <h1 style={{fontSize: "4rem"}}>Find Your Recipe Here</h1>
+          <p>
+            Discover a variety of recipes to suit every taste and preference.
+            Customize your meal plan and enjoy delicious, healthy meals every
+            day.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
