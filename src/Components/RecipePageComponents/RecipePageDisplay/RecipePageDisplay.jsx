ebddb0d080c91809e4886faa54e95f510d0c0ec1
Aspasija <aspasija.cvetkoska@result.si>
2024-07-08 13:42:21 +0200

    added recipe display in the recipe page

--- src/Components/RecipePageComponents/RecipePageDisplay/RecipePageDisplay.jsx
+++ src/Components/RecipePageComponents/RecipePageDisplay/RecipePageDisplay.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import { Col, Form, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import HealthyDish from "../../../Images/zucchini-pasta-red-sauce-sauteed-vegetables-zucchini-carrot-onion-garlic-tomatoes.jpg";
 
 const RecipePageDisplay = () => {
   return (
-    <div>RecipePageDisplay</div>
-  )
-}
+    <div style={{ marginRight:"20px" }}>
+        <Form className="w-100 mb-3">
+        <Row className="gx-2">
+          <Col>
+            <Form.Control type="text" placeholder="Search" />
+          </Col>
+          <Col xs="auto" className="d-flex justify-content-between align-items-center gap-2">
+            <Button type="submit">Search</Button>
+            <Button type="submit" variant="warning">Advanced Search</Button>
+          </Col>
+        </Row>
+      </Form>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          alignItems: "center",
+          fontFamily: "'Oswald', sans-serif",
+          gap:"10px",
+        }}
+      >
+        <Card style={{ width: "400px", marginBottom: "10px" }}>
+          <Card.Img variant="top" src={HealthyDish} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "400px", marginBottom: "10px" }}>
+          <Card.Img variant="top" src={HealthyDish} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "400px", marginBottom: "10px" }}>
+          <Card.Img variant="top" src={HealthyDish} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "400px", marginBottom: "10px" }}>
+          <Card.Img variant="top" src={HealthyDish} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "400px", marginBottom: "10px" }}>
+          <Card.Img variant="top" src={HealthyDish} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "400px", marginBottom: "10px" }}>
+          <Card.Img variant="top" src={HealthyDish} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
-export default RecipePageDisplay
+export default RecipePageDisplay;
