e24f9d957a70dd4d90793206b5b67fa9940ba4bc
Aspasija <aspasija.cvetkoska@result.si>
2024-07-09 12:33:30 +0200

    fetched and displayed popular recipes and added a cookie for the browser

--- src/Components/HomePageComponents/RecipesHomePageComponent/RecipesHomePage.jsx
+++ src/Components/HomePageComponents/RecipesHomePageComponent/RecipesHomePage.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import HealthyDish from "../../../Images/zucchini-pasta-red-sauce-sauteed-vegetables-zucchini-carrot-onion-garlic-tomatoes.jpg";
 
 const RecipesHomePage = () => {
+  const [popularRecipes, setPopularRecipes] = useState([]);
+
+  useEffect(() => {
+    getPopularRecipes();
+  }, []);
+
+  const getPopularRecipes = async () => {
+    const check = localStorage.getItem("recipes");
+
+    if (check) {
+      setPopularRecipes(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=6`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("recipes", JSON.stringify(data.recipes));
+      console.log(data);
+      setPopularRecipes(data.recipes);
+    }
+  };
+
+  const truncateSummary = (summary) => {
+    const cleanSummary = summary.replace(/<[^>]+>/g, "");
+    const words = cleanSummary.split(" ");
+    if (words.length > 100) {
+      return words.slice(0, 13).join(" ") + "...";
+    }
+    return cleanSummary;
+  };
+
   return (
     <div style={{ margin: "20px", fontFamily: "'Oswald', sans-serif" }}>
       <h2
@@ -41,72 +72,20 @@ const RecipesHomePage = () => {
           fontFamily: "'Oswald', sans-serif",
         }}
       >
-        <Card style={{ width: "25rem", margin: "10px" }}>
-          <Card.Img variant="top" src={HealthyDish} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text style={{ fontWeight: "lighter" }}>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "25rem", margin: "10px" }}>
-          <Card.Img variant="top" src={HealthyDish} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text style={{ fontWeight: "lighter" }}>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "25rem", margin: "10px" }}>
-          <Card.Img variant="top" src={HealthyDish} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text style={{ fontWeight: "lighter" }}>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "25rem", margin: "10px" }}>
-          <Card.Img variant="top" src={HealthyDish} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text style={{ fontWeight: "lighter" }}>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "25rem", margin: "10px" }}>
-          <Card.Img variant="top" src={HealthyDish} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text style={{ fontWeight: "lighter" }}>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "25rem", margin: "10px" }}>
-          <Card.Img variant="top" src={HealthyDish} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text style={{ fontWeight: "lighter" }}>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        {popularRecipes.map((recipe) => (
+          <Card style={{ width: "25rem", margin: "10px" }} key={recipe.id}>
+            <Card.Img variant="top" src={recipe.image} />
+            <Card.Body>
+              <Card.Title>{recipe.title}</Card.Title>
+              <Card.Text style={{ fontWeight: "lighter" }}>
+                {truncateSummary(recipe.summary)}
+              </Card.Text>
+              <Button variant="primary" href={recipe.sourceUrl} target="_blank">
+                View Recipe
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
     </div>
   );
